99c243c235b61406ab44e03cc3b168f93fafe754
Andrew Sas <andrewfsas@gmail.com>
2025-01-17 14:36:31 -0500

    Lab 1 coding questions

---
+++ week01/lab1.py
@@ -0,0 +1,18 @@
+# Sample Coding Questions 01 Week 01
+# Andrew Sas
+
+arr = [1, 4, 7, 9]
+
+a = 1
+b = 2
+c = 3
+d = 4
+
+e = a - ((b ** c) // d) + (a % c)
+
+temperature = 32.6
+print("The temperature today is: {} degrees Celsius".format(temperature))
+
+userAge = input("Please input your age: ")
+userAge = int(userAge) + 22
+print("Now showing the show items filtered by age: {}".format(userAge))
